82bcbacfbaaa3ffcc3cb1f433ed0cba5ef434433
Hanaa <hsadoqi@gmail.com>
2021-02-24 19:52:25 -0500

    Validate user creation

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,5 +1,20 @@
 class Api::V1::UsersController < ApplicationController
     def index 
-        render json: UserSerializer.new(User.all)
+        render json: { users: UserSerializer.new(User.all) }
+    end 
+
+    def create 
+        @user = User.create(user_params)
+        if @user.valid?
+            render json: { user: UserSerializer.new(@user) }, status: :created 
+        else 
+            render json: { errors: @user.errors.full_messages }, status: :not_acceptable
+        end 
+    end 
+
+    private 
+
+    def user_params 
+        params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
     end 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,8 @@
 class User < ApplicationRecord
     has_secure_password
+    validates :first_name, :last_name, presence: true, length: { minimum: 2 }
+    validates :email, presence: true, uniqueness: { case_sensitive: false}
+    validates :password, confirmation: true, length: { in: 6..20 }
+    validates :password_confirmation, presence: true
+
 end
